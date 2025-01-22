4b18e0aaf8f975f41b5affb81d9e8c12788021e9
YoungminKim <ggred0123@gmail.com>
2025-01-22 17:24:06 +0900

    report

--- lib/navigation/report/widget.dart
+++ lib/navigation/report/widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
+import 'package:flutter/services.dart';
 import 'package:kaist_map/constant/colors.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ReportTab extends StatefulWidget {
   const ReportTab({super.key});
@@ -35,44 +37,169 @@ class _ReportTabState extends State<ReportTab> {
     }
   }
 
-  // 제보 데이터 처리(서버 전송 등) 함수
-  void _submitReport() {
-    if (_formKey.currentState!.validate()) {
-      // 예시: 데이터 확인
-      final String title = _titleController.text.trim();
-      final String phone = _phoneController.text.trim();
-      final String email = _emailController.text.trim();
-      final String description = _descriptionController.text.trim();
+  final baseUrl =
+      "http://jtkim-loadbalancer-827728116.ap-northeast-2.elb.amazonaws.com";
+  final awsEndpoint = "/aws/s3";
+  final reportEndpoint = "/report";
 
-      // 이곳에서 서버 API 호출 등을 통해 전달
-      // 예: reportService.submitReport(title, phone, email, description, _selectedImages);
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('제보가 접수되었습니다.')),
+  Future<String> _getPresignedUrl(String fileName) async {
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl$awsEndpoint?fileName=$fileName'),
       );
 
-      // 제출 후 초기화
-      _titleController.clear();
-      _phoneController.clear();
-      _emailController.clear();
-      _descriptionController.clear();
-      setState(() {
-        _selectedImages.clear();
-      });
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return data['preSignedUrl'];
+      } else {
+        throw Exception('Failed to get presigned URL: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error getting presigned URL: $e');
+    }
+  }
+
+  Future<void> _uploadImageToS3(File imageFile, String presignedUrl) async {
+    try {
+      final bytes = await imageFile.readAsBytes();
+
+      final response = await http.put(
+        Uri.parse(presignedUrl),
+        body: bytes,
+        headers: {
+          'Content-Type': 'image/jpeg',
+        },
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to upload image to S3: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error uploading image: $e');
+    }
+  }
+
+  // 제보 데이터 처리(서버 전송 등) 함수
+  Future<void> _submitReport() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        // 로딩 표시
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        );
+
+        // 이미지 업로드 후 URL 목록 저장
+        List<String> imageUrls = [];
+
+        // 각 이미지에 대해 업로드 처리
+        for (int i = 0; i < _selectedImages.length; i++) {
+          // 파일명 생성 (고유성을 위해 타임스탬프 추가)
+          String fileName =
+              'report_${DateTime.now().millisecondsSinceEpoch}_$i.jpg';
+
+          // presigned URL 요청
+          final presignedUrlResponse = await http.get(
+            Uri.parse('$baseUrl$awsEndpoint?fileName=$fileName'),
+          );
+
+          if (presignedUrlResponse.statusCode != 200) {
+            throw Exception('Failed to get presigned URL');
+          }
+
+          final urlData = json.decode(presignedUrlResponse.body);
+          final presignedUrl = urlData['preSignedUrl'];
+
+          // S3에 이미지 업로드
+          final bytes = await _selectedImages[i].readAsBytes();
+          final uploadResponse = await http.put(
+            Uri.parse(presignedUrl),
+            body: bytes,
+            headers: {
+              'Content-Type': 'image/jpeg',
+            },
+          );
+
+          if (uploadResponse.statusCode != 200) {
+            throw Exception('Failed to upload image');
+          }
+
+          // 업로드 성공한 이미지 URL 저장
+          imageUrls.add(fileName); // 또는 서버에서 정의한 URL 형식으로 변환
+        }
+
+        // 제보 데이터 준비 (이미지 URL 포함)
+        final Map<String, dynamic> reportData = {
+          'title': _titleController.text.trim(),
+          'phone': _phoneController.text.trim(),
+          'email': _emailController.text.trim(),
+          'description': _descriptionController.text.trim(),
+          'images': imageUrls, // 업로드된 이미지 URL 목록
+        };
+
+        // 제보 데이터 서버로 전송
+        final reportResponse = await http.post(
+          Uri.parse('$baseUrl$reportEndpoint'),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: json.encode(reportData),
+        );
+
+        // 로딩 다이얼로그 닫기
+        Navigator.pop(context);
+
+        if (reportResponse.statusCode == 200) {
+          // 성공 메시지 표시
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('제보가 성공적으로 접수되었습니다.')),
+          );
+
+          // 폼 초기화
+          _titleController.clear();
+          _phoneController.clear();
+          _emailController.clear();
+          _descriptionController.clear();
+          setState(() {
+            _selectedImages.clear();
+          });
+        } else {
+          throw Exception('제보 전송 실패: ${reportResponse.statusCode}');
+        }
+      } catch (e) {
+        // 로딩 다이얼로그 닫기 (에러 발생 시)
+        Navigator.pop(context);
+
+        // 에러 메시지 표시
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('제보 접수 중 오류가 발생했습니다: $e')),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: KMapColors.darkBlue.shade300,
+      backgroundColor: KMapColors.darkBlue.shade100,
       appBar: AppBar(
-        backgroundColor: KMapColors.darkBlue.shade900,
+        backgroundColor: KMapColors.darkBlue.shade100,
         title: const Text('제보하기'),
-        titleTextStyle: const TextStyle(
+        titleTextStyle: TextStyle(
           fontSize: 22,
           fontWeight: FontWeight.bold,
-          color: Colors.white,
+          color: KMapColors.darkBlue.shade900,
+        ),
+        elevation: 0,
+        scrolledUnderElevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.light.copyWith(
+          statusBarColor: Colors.transparent, // 상단 바 배경색 투명하게
+          statusBarIconBrightness: Brightness.dark, // 상단 바 아이콘을 어둡게 (검정색)
         ),
       ),
       body: SingleChildScrollView(
@@ -97,9 +224,22 @@ class _ReportTabState extends State<ReportTab> {
               // 제목
               TextFormField(
                 controller: _titleController,
-                decoration: const InputDecoration(
-                  labelText: '제목',
-                  border: OutlineInputBorder(),
+                decoration: InputDecoration(
+                  hintText: '제목',
+                  hintStyle: TextStyle(
+                    color: Colors.black.withAlpha(100),
+                  ),
+                  border: InputBorder.none,
+                  filled: true, // 배경색을 적용하기 위해 필요
+                  fillColor: KMapColors.darkBlue.shade200,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12), // 모서리 둥글기 설정
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -113,9 +253,22 @@ class _ReportTabState extends State<ReportTab> {
               // 전화번호 (선택)
               TextFormField(
                 controller: _phoneController,
-                decoration: const InputDecoration(
-                  labelText: '전화번호 (선택)',
-                  border: OutlineInputBorder(),
+                decoration: InputDecoration(
+                  hintText: '전화번호(선택)',
+                  hintStyle: TextStyle(
+                    color: Colors.black.withAlpha(100),
+                  ),
+                  border: InputBorder.none,
+                  filled: true, // 배경색을 적용하기 위해 필요
+                  fillColor: KMapColors.darkBlue.shade200,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12), // 모서리 둥글기 설정
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
                 keyboardType: TextInputType.phone,
               ),
@@ -124,9 +277,22 @@ class _ReportTabState extends State<ReportTab> {
               // 이메일 (선택)
               TextFormField(
                 controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: '이메일 (선택)',
-                  border: OutlineInputBorder(),
+                decoration: InputDecoration(
+                  hintText: '이메일(선택)',
+                  hintStyle: TextStyle(
+                    color: Colors.black.withAlpha(100),
+                  ),
+                  border: InputBorder.none,
+                  filled: true, // 배경색을 적용하기 위해 필요
+                  fillColor: KMapColors.darkBlue.shade200,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12), // 모서리 둥글기 설정
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
                 keyboardType: TextInputType.emailAddress,
               ),
@@ -135,9 +301,22 @@ class _ReportTabState extends State<ReportTab> {
               // 설명
               TextFormField(
                 controller: _descriptionController,
-                decoration: const InputDecoration(
-                  labelText: '설명',
-                  border: OutlineInputBorder(),
+                decoration: InputDecoration(
+                  hintText: '내용',
+                  hintStyle: TextStyle(
+                    color: Colors.black.withAlpha(100),
+                  ),
+                  border: InputBorder.none,
+                  filled: true, // 배경색을 적용하기 위해 필요
+                  fillColor: KMapColors.darkBlue.shade200,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12), // 모서리 둥글기 설정
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
                 maxLines: 5,
                 validator: (value) {
